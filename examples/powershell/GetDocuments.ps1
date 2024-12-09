c5bbad3626e9a9a06872fb7bc6cfc9b16db884a5
Björn Axenath <baxenath@phoenixcontact.com>
2024-12-09 14:06:41 +0100

    get shells and select; get documents

---
+++ examples/powershell/GetDocuments.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module .\base64url.psm1
+$headers = @{
+'Ocp-Apim-Subscription-Key' = $subscription
+'Content-Type' = 'application/json'
+}
+
+### Loopup Asset and get AasId
+$assetId = 'https://i4d.de/T/2924016' # MACX MCR-EX-SL-RPSSI-I-SP
+$assetIdEncoded = ConvertTo-Base64Url $assetId
+$assetLookupResponse = Invoke-RestMethod -Method Get -Headers $headers -Uri "https://api.phoenixcontact.com/dt4i/asset-management/aas/v1/lookup/shells?assetIds=$assetIdEncoded" 
+# $assetLookupResponse | ConvertTo-Json -Depth 100
+$aasId = $assetLookupResponse.result[0]
+$aasIdEncoded = ConvertTo-Base64Url $aasId 
+
+### Get AAS by AasId
+$shellsAasResponse = Invoke-RestMethod -Method Get -Headers $headers -Uri "https://api.phoenixcontact.com/dt4i/asset-management/aas/v1/shells/$aasIdEncoded" 
+# $shellsAasResponse | ConvertTo-Json -Depth 100
+
+#### Search SubmodelId of Handover Documentation 
+$handoverDocumentIds = $shellsAasResponse.submodels | Where-Object {$_.referredSemanticId.keys.value -EQ '0173-1#01-AHF578#001'}
+$handoverDocumentationId = $handoverDocumentIds[0].keys.value
+$handoverDocumentationIdEncoded = ConvertTo-Base64Url $handoverDocumentationId
+
+### Get links to Documents
+$subModelResponse = Invoke-RestMethod -Method Get -Headers $headers -Uri "https://api.phoenixcontact.com/dt4i/asset-management/aas/v1/submodels/$handoverDocumentationIdEncoded" 
+# $subModelResponse | ConvertTo-Json -Depth 100
+
+$files = $subModelResponse.submodelElements.value.value | Where-Object {$_.idShort -like 'DigitalFile*'} | select-object -Property value
+$files # $files | ForEach-Object {Invoke-WebRequest -Uri  $_.value -OutFile "c:\temp\downloadFiles" -Headers $headers}
---
+++ examples/powershell/base64url.psm1
@@ -0,0 +1,93 @@
+﻿
+# https://www.powershellgallery.com/packages/Posh-ACME/4.20.0/Content/Private%5CConvertTo-Base64Url.ps1
+
+function ConvertTo-Base64Url {
+    [CmdletBinding()]
+    [OutputType('System.String')]
+    param(
+        [Parameter(ParameterSetName='String',Mandatory,Position=0,ValueFromPipeline)]
+        [AllowEmptyString()]
+        [string]$Text,
+        [Parameter(ParameterSetName='String')]
+        [switch]$FromBase64,
+        [Parameter(ParameterSetName='Bytes',Mandatory,Position=0)]
+        [AllowEmptyCollection()]
+        [byte[]]$Bytes
+    )
+
+    Process {
+
+        if (-not $FromBase64) {
+
+            # get a byte array from the input string
+            if ($PSCmdlet.ParameterSetName -eq 'String') {
+                $Bytes = [Text.Encoding]::UTF8.GetBytes($Text)
+            }
+
+            # standard base64 encoder
+            $s = [Convert]::ToBase64String($Bytes)
+
+        } else {
+            # $Text is already Base64 encoded, we just need the Url'ized version
+            $s = $Text
+        }
+
+        # remove trailing '='s
+        $s = $s.Split('=')[0]
+
+        # 62nd and 63rd char of encoding
+        $s = $s.Replace('+','-').Replace('/','_')
+
+        return $s
+
+    }
+
+}
+
+# https://www.powershellgallery.com/packages/Posh-ACME/4.20.0/Content/Private%5CConvertFrom-Base64Url.ps1
+
+function ConvertFrom-Base64Url {
+    [CmdletBinding()]
+    [OutputType('System.String', ParameterSetName='String')]
+    [OutputType('System.Byte[]', ParameterSetName='Bytes')]
+    param(
+        [Parameter(ParameterSetName='Bytes',Mandatory,Position=0,ValueFromPipeline)]
+        [Parameter(ParameterSetName='String',Mandatory,Position=0,ValueFromPipeline)]
+        [AllowEmptyString()]
+        [string]$Base64Url,
+        [Parameter(ParameterSetName='Bytes',Mandatory)]
+        [switch]$AsByteArray
+    )
+
+    Process {
+
+        # short circuit on empty strings
+        if ($Base64Url -eq [string]::Empty) {
+            return [string]::Empty
+        }
+
+        # put the standard unsafe characters back
+        $s = $Base64Url.Replace('-', '+').Replace('_', '/')
+
+        # put the padding back
+        switch ($s.Length % 4) {
+            0 { break; }             # no padding needed
+            2 { $s += '=='; break; } # two pad chars
+            3 { $s += '='; break; }  # one pad char
+            default { throw "Invalid Base64Url string" }
+        }
+
+        # convert it using standard base64 stuff
+        if ($AsByteArray) {
+            return [Convert]::FromBase64String($s)
+        } else {
+            return [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($s))
+        }
+
+    }
+
+}
+
+function Clear-AllUserVariables{
+  Get-Variable | Where-Object { $_.Options -notmatch "Constant|ReadOnly" } | ForEach-Object { Remove-Variable -Name $_.Name }
+}
